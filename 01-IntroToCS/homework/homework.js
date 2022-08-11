7d18e4beb8c608bd30dde44c5a48b2a6dc656daf
MatiasIvanM <matiasivan.sd@gmail.com>
2022-08-11 19:07:02 -0300

    save repo

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,31 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca .split("").reverse();
+
+// return (num).toString(2);
+
+}
+
+
+function DecimalABinario(num) {
+  // tu codigo aca usar While
+/*var num = 23;
+  var modulo = []
+  while(num !==0);{
+   
+  var modulo = num % 2;
+  num = Math.floor(num/2);
+  }
+   return modulo;
+  */
+
+
+   
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
