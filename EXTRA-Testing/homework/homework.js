86c378f9929ccd5b63d5ae0208dfc0affeae4649
MatiasIvanM <matiasivan.sd@gmail.com>
2022-11-07 22:51:21 -0300

    Testisng correction

---
+++ EXTRA-Testing/homework/homework.js
@@ -0,0 +1,46 @@
+const layout = [
+    [{type: 'VIP', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}],
+    [{type: 'NORMAL', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}, {type: 'NORMAL', booked: false}],
+    [{type: 'NORMAL', booked: false}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: false}],
+    [{type: 'ECONOMIC', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'ECONOMIC', booked: false}],
+    [{type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: true}, {type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: false}]
+  ];
+
+
+
+  function checkSeatStatus(row, number) {
+    if(typeof row !== 'string') throw new TypeError('First parameter is not a string');
+    if(typeof number !== 'number') throw new TypeError('Second parameter is not a number');
+  
+    let rowIndex = getRowNumber(row);
+    let layoutRows = layout[rowIndex];
+    let seat = layoutRows[number];
+    return seat.booked;
+  }
+
+
+
+function getRowNumber(letter) {
+return letter.charCodeAt() -65;
+}
+
+
+function book(row, col){
+    if(checkSeatStatus (row, col))
+    return 'Seat in ' + row + col + ' is already booked';
+    let rowIndex = getRowNumber(row);
+    let column = layout [rowIndex];
+    let seat = column[col];
+    seat.booked =true;
+
+    return 'Seat in ' +row + col + ' successfully booked';
+};
+
+
+module.exports = {
+    checkSeatStatus,
+    getRowNumber,
+    book
+  };
+
+
---
+++ EXTRA-Testing/homework/tests/index.test.js
@@ -0,0 +1,54 @@
+
+const { checkSeatStatus, getRowNumber,book } = require('../homework');
+//const checkSeatStatus = require('../homework.js');
+
+describe('checkSeatStatus', function () {
+        it('checkSeatStatus should be a function', function () {
+            expect(typeof checkSeatStatus).toBe('function');
+        });
+
+        it('should throw a TypeError if second parameter is not a string', function () {
+            expect(() => {
+                return checkSeatStatus(9);
+            }).toThrow(new TypeError('First parameter is not a string'));
+        });
+
+        it('should throw a TypeError if second parameter is not a number', function () {
+            expect(() => {
+                return checkSeatStatus('hola', '9');
+            }).toThrow(new TypeError('Second parameter is not a number'));
+        });
+    });
+
+ describe('getRowNumber', () => {
+  it('should return 1 if the letter given is an A', function () {
+          expect(getRowNumber('A')).toBe(0);
+      });
+  
+  it('should return 3 if the letter given is a C', function () {
+          expect(getRowNumber('C')).toBe(2);
+      });
+
+
+  it('should return true if the given seat defined by row and column is booked', function () {
+          expect(checkSeatStatus('A', 1)).toBe(true);
+      });
+  
+  it('should return false if the given seat defined by row and column is not booked', function () {
+          expect(checkSeatStatus('E', 3)).toBe(false);
+      });
+ });
+
+
+  describe('book', () => {
+
+    it('should return "Seat in XX successfully booked" if the given seat is not booked', function () {
+            expect(book('E', 3)).toBe('Seat in E3 successfully booked');
+        });
+    
+    it('should return "Seat in XX is already booked" if the given seat is already booked', function () {
+            expect(book('A', 1)).toBe('Seat in A1 is already booked');
+        });
+  });
+
+  
